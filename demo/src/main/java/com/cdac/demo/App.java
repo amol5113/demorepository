6382d1a44afbbd4afedf11bd69554060bd9997bc
Amol Gawade <amolg@cdac.in>
2019-12-20 15:17:34 +0530

    first push

---
+++ demo/src/main/java/com/cdac/demo/App.java
@@ -0,0 +1,15 @@
+package com.cdac.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(" creating new repository");
+        
+    }
+}
